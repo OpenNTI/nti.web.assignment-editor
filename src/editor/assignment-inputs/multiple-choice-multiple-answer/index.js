7aa2c5e07347114402563b07d0cead97a1e2c804
Andrew Ligon <andrew.ligon@nextthought.com>
2016-05-18 19:04:07 -0500

    Mixed up mime types

--- src/editor/assignment-inputs/multiple-choice-multiple-answer/index.js
+++ src/editor/assignment-inputs/multiple-choice-multiple-answer/index.js
@@ -6,8 +6,9 @@ import base from '../base';
 export default class MultipleChoiceMultipleAnswerPart extends base {
 	//TODO: get this mime type from the model
 	static handles = [
-		'application/vnd.nextthought.assessment.multiplechoicepart',
-		'application/vnd.nextthought.assessment.randomizedmultiplechoicepart'
+		'application/vnd.nextthought.assessment.multiplechoicemultipleanswerpart',
+		'application/vnd.nextthought.assessment.randomizedmultiplechoicemultipleanswerpart'
+
 	]
 
 
