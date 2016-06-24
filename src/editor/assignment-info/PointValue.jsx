a33746dcfb342da162938eb716e685dc9c6c62b3
Ray Hatfield <ray.hatfield@nextthought.com>
2016-06-23 21:58:24 -0500

    initial pass on point value editing

--- src/editor/assignment-info/PointValue.jsx
+++ src/editor/assignment-info/PointValue.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import {NumberInput} from 'nti-web-commons';
 
 export default class PointValue extends React.Component {
 	constructor (props) {
 		super(props);
 
 		this.state = {};
+		this.onBlur = this.onBlur.bind(this);
+	}
+
+	static propTypes = {
+		assignment: React.PropTypes.object.required
+	}
+
+	onBlur () {
+		const {assignment} = this.props;
+		const {value} = this.input;
+		console.log(value);
+		assignment.save({
+			PointValue: value
+		});
 	}
 
 
 	render () {
+
+		const {value} = this.state;
+
 		return (
 			<div className="field point-value">
 				<div className="field-label">Point Value</div>
+				<div className="field-value">
+					<NumberInput ref={x => this.input = x} onBlur={this.onBlur} defaultValue={value} />
+				</div>
 			</div>
 		);
 	}
