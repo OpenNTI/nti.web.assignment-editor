23d0e9cac3e64663f40aea7020ebebe10373d0ef
Ray Hatfield <ray.hatfield@nextthought.com>
2016-06-27 13:17:19 -0500

    save point value changes

--- src/editor/assignment-info/PointValue.jsx
+++ src/editor/assignment-info/PointValue.jsx
@@ -7,20 +7,56 @@ export default class PointValue extends React.Component {
 
 		this.state = {};
 		this.onBlur = this.onBlur.bind(this);
+		this.onChange = this.onChange.bind(this);
+		this.save = this.save.bind(this);
+		this.setUp = this.setUp.bind(this);
 	}
 
 	static propTypes = {
 		assignment: React.PropTypes.object.isRequired
 	}
 
-	onBlur () {
-		const {assignment} = this.props;
-		const {value} = this.input;
-		assignment.save({
-			PointValue: value
+	componentWillMount () {
+		this.setUp();
+	}
+
+	componentWillReceiveProps (nextProps) {
+		this.setUp(nextProps);
+	}
+
+	setUp (props = this.props) {
+		const {assignment} = props;
+		const value = assignment.totalPoints;
+		this.setState({
+			value
 		});
 	}
 
+	onBlur () {
+		this.save();
+	}
+
+	onChange (e) {
+		this.setState({
+			value: e.target.value,
+			changed: true
+		});
+	}
+
+	save () {
+		const {assignment} = this.props;
+		const {value, changed} = this.state;
+		if(changed) {
+			assignment.save({
+				'total_points': value || 0
+			})
+			.then(() => {
+				this.setState({
+					changed: false
+				});
+			});
+		}
+	}
 
 	render () {
 
@@ -29,7 +65,7 @@ export default class PointValue extends React.Component {
 		return (
 			<div className="field point-value">
 				<LabeledValue label="Value">
-					<NumberInput ref={x => this.input = x} onBlur={this.onBlur} defaultValue={value} />
+					<NumberInput ref={x => this.input = x} onBlur={this.onBlur} onChange={this.onChange} defaultValue={value} />
 				</LabeledValue>
 			</div>
 		);
