0105ba912f65895d5d6ed31f8ee42f7bed70daac
Ray Hatfield <ray.hatfield@nextthought.com>
2016-06-28 15:58:13 -0500

    paint file upload question type

--- src/editor/assignment-inputs/file-upload/Editor.jsx
+++ src/editor/assignment-inputs/file-upload/Editor.jsx
@@ -5,17 +5,19 @@ export default class FileUploadEditor extends React.Component {
 		part: React.PropTypes.object.isRequired
 	}
 
-
 	constructor (props) {
 		super(props);
 
 		this.state = {};
 	}
 
-
 	render () {
 		return (
-			<div>TODO: Fill Out File Upload Type</div>
+			<div className="file-upload">
+				<div>Upload your file here.</div>
+				<div>Maximum file size is 10MB.</div>
+				<div className="upload-button">Upload a file</div>
+			</div>
 		);
 	}
 }
