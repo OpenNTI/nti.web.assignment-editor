00ef49b78667ae989d79bdc459c31c473eb2ca53
Josh Birdwell <jbirdwell34@gmail.com>
2016-06-29 14:23:17 -0500

    Look for the publish links to deteremine if to show Publish Lock
    Added a logger. Enabled the delete of the edit link. Permissions for enabling delete need to be looked over again.

--- src/editor/controls/PublishControls.jsx
+++ src/editor/controls/PublishControls.jsx
@@ -1,11 +1,13 @@
 import React, {PropTypes} from 'react';
 import {Publish, Constants} from 'nti-web-commons';
 import {HOC} from 'nti-web-commons';
-
+import Logger from 'nti-util-logger';
 import PublishLocked from './PublishLocked';
+
 
 const {ItemChanges} = HOC;
 const {PUBLISH_STATES} = Constants;
+const logger = Logger.get('lib:asssignment-editor:publish-controls');
 
 class PublishControls extends React.Component {
 	static propTypes = {
@@ -27,22 +29,29 @@ class PublishControls extends React.Component {
 
 		const state = PublishStateMap[value] || (value instanceof Date ? value : void value);
 
-		return assignment.setPublishState(state);
+		return assignment.setPublishState(state)
+			.catch((err) => logger.error(err.stack || err.message || err));
 	}
 
 
 	render () {
 		const assignment = PublishControls.getItem(this.props);
-		const value = Publish.evaluatePublishStateFor(assignment);
-		const canDelete = assignment.hasLink('edit');
+		const value = Publish.evaluatePublishStateFor({
+			isPublished: () => assignment.isPublished() && assignment.getPublishDate() < Date.now(),
+			getPublishDate: () => assignment.getPublishDate()
+		});
 
-		const Control = false ? PublishLocked : Publish;
+		const canDelete = assignment.hasLink('edit');
+		const isPublishable = assignment.hasLink('publish') || assignment.hasLink('unpublish');
+		const Control = isPublishable ? Publish : PublishLocked;
 
 		return (
 			<div className="assignment-publish-controls">
 				<Control alignment="top-right"
 					value={value}
 					onChange={this.onChange}
+					assignment={assignment}
+					enableDelete={canDelete}
 				/>
 			</div>
 		);
