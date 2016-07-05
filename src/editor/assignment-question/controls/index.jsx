9c0420e90749810a3fe03a62f41bc733243cd74f
Andrew Ligon <andrew.ligon@nextthought.com>
2016-07-05 16:45:28 -0500

    Don't allow moving a question up or down if the question set doesn't have an ordered contents link

--- src/editor/assignment-question/controls/index.jsx
+++ src/editor/assignment-question/controls/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 
 import {hasOrderedContents} from '../../utils/OrderedContents';
 
@@ -17,15 +18,18 @@ export default function QuestionControls (props) {
 	const {question, questionSet, assignment} = props;
 
 	const ordered = hasOrderedContents(questionSet);
+	const canMove = ordered;
 	const canDelete = ordered;
 	const canDuplicate = ordered;
+	const isEmpty = !canMove && !canDelete && !canDuplicate;
+	const cls = cx('assignment-editing-controls', {empty: isEmpty});
 
 	return (
-		<div className="assignment-editing-controls">
-			<Move type="up" question={question} questionSet={questionSet} assignment={assignment} />
-			<Move type="down" question={question} questionSet={questionSet} assignment={assignment} />
-			{canDuplicate && ( <Duplicate question={question} questionSet={questionSet} assignment={assignment} /> )}
-			{canDelete && ( <Delete question={question} questionSet={questionSet} assignment={assignment} /> )}
+		<div className={cls}>
+			{canMove && (<Move type="up" question={question} questionSet={questionSet} assignment={assignment} />)}
+			{canMove && (<Move type="down" question={question} questionSet={questionSet} assignment={assignment} />)}
+			{canDuplicate && (<Duplicate question={question} questionSet={questionSet} assignment={assignment} />)}
+			{canDelete && (<Delete question={question} questionSet={questionSet} assignment={assignment} />)}
 		</div>
 	);
 }
