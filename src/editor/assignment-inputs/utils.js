181059eabda153e5d764df3000e9316df65a07b6
Andrew Ligon <andrew.ligon@nextthought.com>
2016-07-05 22:41:19 -0500

    Placeholders don't have a get link method, so make sure its defined before its called.

--- src/editor/assignment-inputs/utils.js
+++ src/editor/assignment-inputs/utils.js
@@ -1,11 +1,15 @@
+function getLink (question, name) {
+	return question && question.getLink && question.getLink(name);
+}
+
 export function canAddPart (question) {
-	return question.getLink('InsertPart');
+	return getLink(question, 'InsertPart');
 }
 
 export function canMovePart (question) {
-	return question.getLink('MovePart');
+	return getLink(question, 'MovePart');
 }
 
 export function canRemovePart (question) {
-	return question.getLink('RemovePart');
+	return getLink(question, 'RemovePart');
 }
