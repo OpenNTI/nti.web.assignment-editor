e5fa5ef87b04f4b3f6a6cee780e0338aa2959fa5
Andrew Ligon <andrew.ligon@nextthought.com>
2016-07-05 23:11:14 -0500

    Fix typo in the default ordering question parts.

--- src/editor/assignment-inputs/ordering/Button.jsx
+++ src/editor/assignment-inputs/ordering/Button.jsx
@@ -7,7 +7,7 @@ import {generatePartFor} from './utils';
 const ICON_CLS = 'ordering';
 
 const defaultLabels = ['Label 1', 'Label 2'];
-const defaultValues = ['Value 1', 'Label 2'];
+const defaultValues = ['Value 1', 'Value 2'];
 const defaultSolution = {'0': 0, '1': 1};
 
 const DEFAULT_TEXT = {
