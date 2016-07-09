93b39e0a8377fa207b4e51e0afdb453247740d95
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-09 16:04:49 -0500

    make AutoGrade option Optimistic and report errors.

--- src/editor/assignment-options/options/Grading.jsx
+++ src/editor/assignment-options/options/Grading.jsx
@@ -16,26 +16,72 @@ export default class Grading extends React.Component {
 		assignment: PropTypes.object.isRequired
 	}
 
+	constructor (props) {
+		super();
+
+		this.setupValue(props);
+	}
+
+	componentDidUpdate () {
+		const {assignment} = this.props;
+		const {isAutoGraded} = this.state;
+
+		if (assignment.isAutoGraded !== isAutoGraded) {
+			this.save();
+		}
+	}
+
+
+	componentWillReceiveProps (nextProps) {
+		if (this.props.assignment !== nextProps.assignment) {
+			this.setupValue(nextProps);
+		}
+	}
+
+
+	save = () => {
+		const {assignment} = this.props;
+		const {isAutoGraded} = this.state;
+
+		if (assignment.isAutoGraded !== isAutoGraded) {
+			assignment.setAutoGrade(isAutoGraded)
+				.catch(error => {
+					this.setState({isAutoGraded: assignment.isAutoGraded, error});
+				});
+		}
+	}
+
+
+	setupValue (props = this.props) {
+		const setState = s => this.state ? this.setState(s) : (this.state = s);
+		const {assignment: {isAutoGraded}} = props;
+
+		setState({ isAutoGraded });
+	}
+
+
+	onItemChanged = () => {
+		this.setupValue();
+	}
+
 
 	onChange = () => {
-		if (this.busy) { return; }
-		this.busy = true;
-
-		const {assignment} = this.props;
-		const {isAutoGraded} = assignment;
-		const clearBusy = () => delete this.busy;
-
-		assignment.setAutoGrade(!isAutoGraded)
-			.then(clearBusy, clearBusy);
+		const {isAutoGraded} = this.state;
+		this.setState({isAutoGraded: !isAutoGraded, error: null});
 	}
+
 
 	render () {
 		const {assignment} = this.props;
-		const {isAutoGraded} = assignment;
+		const {isAutoGraded, error} = this.state;
 		const disabled = !assignment.hasLink('edit');
 
+		const errorMessage = error && (
+			error.message
+		);
+
 		return (
-			<OptionGroup name="grading" header="Grading" content={t('content')}>
+			<OptionGroup name="grading" header="Grading" content={t('content')} error={errorMessage}>
 				<Option label={t('label')} name="auto-grading" value={isAutoGraded} onChange={this.onChange} disabled={disabled}/>
 			</OptionGroup>
 		);
