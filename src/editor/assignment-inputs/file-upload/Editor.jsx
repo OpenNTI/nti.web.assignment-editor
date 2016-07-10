35a8b57ff54da0f6cb2e42b3ed568a4e2d9aff68
pacimahoro <pacimahoro@gmail.com>
2016-07-10 18:54:22 -0500

    Fix NTI-1436: Reflect the allowed file types in the upload text.

--- src/editor/assignment-inputs/file-upload/Editor.jsx
+++ src/editor/assignment-inputs/file-upload/Editor.jsx
@@ -35,13 +35,33 @@ export default class FileUploadEditor extends React.Component {
 		return true;
 	}
 
-	render () {
+	getExtensionText (extensions) {
+		let txt;
+		extensions = extensions || [];
+		if (extensions.length > 1) {
+			extensions = extensions.slice();
 
+			const last = extensions.pop();
+			txt = extensions.join(', ') + ' or ' + last;
+			txt = 'Upload your ' + txt + ' here.';
+		}
+		else if (extensions.length === 1 && extensions[0] !== '*.*') {
+			txt = 'Upload your ' + extensions[0] + ' here.';
+		}
+		else {
+			txt = 'Upload your file here.';
+		}
+
+		return txt;
+	}
+
+	render () {
 		const {part} = this.props;
+		const title = this.getExtensionText(part && part.allowed_extensions);
 
 		return (
 			<div className="file-upload">
-				<div>Upload your file here.</div>
+				<div>{title}</div>
 				{part.max_file_size && <div>Maximum file size is <span className="filesize">{filesize(part.max_file_size)}</span>.</div>}
 				<div className="upload-button">Upload a file</div>
 				<SettingsButton onClick={this.showSettings} />
