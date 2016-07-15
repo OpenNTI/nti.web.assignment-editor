c3a4c5a9740c54dc9f71209640aa6d3383df0ba9
Josh Birdwell <jbirdwell34@gmail.com>
2016-07-15 16:06:21 -0500

    NTI-1616 -  Update no parts

--- src/editor/assignment-parts/components/NoParts.jsx
+++ src/editor/assignment-parts/components/NoParts.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import autobind from 'nti-commons/lib/autobind';
+import {scoped} from 'nti-lib-locale';
 
 import {Dropzone} from '../../../dnd';
 
 import {createPartWithQuestion} from '../Actions';
 import {QUESTION_TYPE} from './QuestionSet';
 
-const PLACEHOLDER = 'No questions yet. Select question from the right to add one.';
+
+const DEFAULT_TEXT = {
+	placeholder: 'No questions yet. Select question from the right to add one.'
+};
+
+const t = scoped('NO_PARTS', DEFAULT_TEXT);
 
 export default class NoParts extends React.Component {
 	static propTypes = {
@@ -37,7 +43,7 @@ export default class NoParts extends React.Component {
 		return (
 			<Dropzone className="assignment-editor-no-parts" accepts={this.accepts} dropHandlers={this.dropHandlers}>
 				<div>
-					{PLACEHOLDER}
+					{t('placeholder')}
 				</div>
 			</Dropzone>
 		);
