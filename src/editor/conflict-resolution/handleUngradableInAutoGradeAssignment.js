6e4a860544336d9044579b32c51e97f0e030532a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-10-13 17:04:22 -0500

    cleanup. use wait.on()

--- src/editor/conflict-resolution/handleUngradableInAutoGradeAssignment.js
+++ src/editor/conflict-resolution/handleUngradableInAutoGradeAssignment.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ConflictResolution, Prompt} from 'nti-web-commons';
+import {ConflictResolution, Prompt, wait} from 'nti-web-commons';
 
 import Store from '../Store';
 
@@ -13,11 +13,12 @@ export default function handleUngradableInAutoGradeAssignment (challenge) {
 
 	const {assignment} = Store;
 
+	const refresh = () => assignment && assignment.refresh().then(() => assignment.onChange('refreshed'));
+
 	return new Promise((confirm, reject) => {
-		const onCancel = () => (assignment && assignment.refresh())
-							.then(() => assignment.onChange('refreshed'))
-							.then(() => (challenge.reject(), reject()))
-							.catch(() => (challenge.reject(), reject()));
+
+		const onCancel = () => assignment && wait.on(refresh())
+								.then(() => (challenge.reject(), reject()));
 
 		Prompt.modal(
 			<DefaultConfirmPrompt challenge={challenge} onConfirm={confirm} onCancel={onCancel}/>,
@@ -25,6 +26,5 @@ export default function handleUngradableInAutoGradeAssignment (challenge) {
 		);
 	})
 		.then(() => challenge.confirm())
-		.then(() => assignment && assignment.refresh())
-		.then(() => assignment && assignment.onChange('refreshed'));
+		.then(refresh);
 }
