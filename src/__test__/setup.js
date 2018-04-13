cd6a53e4eddaf6cf6aaf2d85f920d4aaae26bc89
Cory Jones <cory.jones@nextthought.com>
2018-04-13 16:13:50 -0500

    Move assignment inline publish/dates editor into assignment component

--- src/__test__/setup.js
+++ src/__test__/setup.js
@@ -1,2 +1,5 @@
 /*eslint-disable*/
 require('raf/polyfill');
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+Enzyme.configure({ adapter: new Adapter() });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export Editor from './editor';
 // export Question from './question';
-
+export InlineEditor from './editor/inline';
 export ControlBar from './components/ControlBar';
