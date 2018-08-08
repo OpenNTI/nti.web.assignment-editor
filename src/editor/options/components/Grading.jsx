9e64418611a01db99440d92821cea95584337cbc
Cory Jones <cory.jones@nextthought.com>
2018-08-08 13:13:02 -0500

    Update wording

--- src/editor/options/components/Grading.jsx
+++ src/editor/options/components/Grading.jsx
@@ -12,8 +12,8 @@ const DEFAULT_TEXT = {
 	'disabled-no-questions': 'Add some questions to enable this option.',
 	'disabled-total-points': 'Assignment must have a point value to enable auto grading.',
 	'disabled-conflicting-questions': {
-		other: 'Questions without provided solutions are not compatible with auto grading. You must remove questions %(conflicts)s before enabling auto grade.',
-		one: 'Questions without provided solutions are not compatible with auto grading. You must remove question %(conflicts)s before enabling auto grade.'
+		other: 'Questions without provided answers are not compatible with auto grading. You must remove questions %(conflicts)s before enabling auto grade.',
+		one: 'Questions without provided answers are not compatible with auto grading. You must remove question %(conflicts)s before enabling auto grade.'
 	}
 };
 
