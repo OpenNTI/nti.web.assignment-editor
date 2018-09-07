575eb84315a4ac30c563a8222677ab1ebe3690fc
Jonathan Grimes <jsg2021@gmail.com>
2018-09-07 16:53:33 -0500

    update style imports

--- test/app/index.js
+++ test/app/index.js
@@ -8,11 +8,6 @@ import {decodeFromURI} from '@nti/lib-ntiids';
 
 import {Editor} from '../../src';
 
-// import 'normalize.css';
-import '@nti/style-common/all.scss';
-import '@nti/web-commons/lib/index.css';
-import '@nti/web-editor/index.css';
-import '@nti/web-modeled-content/lib/index.css';
 
 window.$AppConfig = window.$AppConfig || {server: '/dataserver2/'};
 
