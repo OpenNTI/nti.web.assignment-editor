e533c56e2e09989ce2e0e6dfd40cacdd964d3130
Ray Hatfield <ray.hatfield@gmail.com>
2021-07-01 09:56:58 -0500

    NTI-10788 - show spinner instead of placeholder when item is an ntiid

--- src/editor/assignment-parts/components/Part.jsx
+++ src/editor/assignment-parts/components/Part.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Loading } from '@nti/web-commons';
+
 import QuestionSet from './QuestionSet';
 import Placeholder from './PlaceholderQuestionSet';
 
@@ -10,19 +12,25 @@ AssignmentPart.propTypes = {
 	course: PropTypes.object,
 };
 
+const Spinner = styled(Loading.Spinner)`
+	margin: 6rem auto;
+`;
+
 export default function AssignmentPart({
 	part: { question_set: questionSet },
 	assignment,
 	course,
 }) {
-	// If questionSet is a string (ntiid) we wait, on the assumption that
-	// it's an ntiid and will be resolved in a subsequent render. See NTI-10788
-	// for an edge case where this occurs.
-	if (!assignment.isModifiable || typeof questionSet === 'string') {
+	if (!assignment.isModifiable) {
 		return <Placeholder />;
 	}
 
-	return (
+	// If questionSet is a string (ntiid) we wait, on the assumption that
+	// it's an ntiid and will be resolved in a subsequent render. See NTI-10788
+	// for an edge case where this occurs.
+	return typeof questionSet === 'string' ? (
+		<Spinner />
+	) : (
 		<QuestionSet
 			questionSet={questionSet}
 			assignment={assignment}
